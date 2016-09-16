ecb8b5668e9462d0771d5257d005e09aa73d9977
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-09-16 10:42:37 -0700

    successfully sending requests

--- script.js
+++ script.js
@@ -17,28 +17,28 @@
 var $ = {
 
   ajax: function(options) {
-    var request = {}
-    var request.complete = options.complete || function() { };
-    var request.data = options.data || {};
-    var request.dataType = options.dataType || "json"
-    var request.error = options.error || function() { };
-    var request.headers = options.headers || function() { };
-    var request.method = options.method || "GET";
-    var request.success = options.success || function() {};
-    var request.url = options.url ;
-    var request.async = options.async || true;
+    var request = {};
+    request.complete = options.complete || function() { };
+    request.data = options.data || {};
+    request.dataType = options.dataType || "json";
+    request.error = options.error || function() { };
+    request.headers = options.headers || function() { };
+    request.method = options.method || "GET";
+    request.success = options.success || function() {};
+    request.url = options.url ;
+    request.async = options.async || true;
 
     var xhr = new XMLHttpRequest();
 
-    this.errorListener(xhr, xhrequest)
-    this.successListener(xhr, request)
+    this.errorListener(xhr, request);
+    this.loadListener(xhr, request);
 
 
 
-    xhr.open(options.method, options.url, options.async);
+    xhr.open(request.method, request.url, request.async);
     xhr.send();
 
-    return
+    return xhr;
 
   },
 
@@ -48,15 +48,32 @@ var $ = {
     });
   },
 
-  successListener: function(xhr, requestOptions) {
-      xhr.addEventListener( "error", function(){
-        return requestOptions.success(JSON.parse(xhr.data), xhr.status, xhr.statusText);
+  loadListener: function(xhr, requestOptions) {
+      xhr.addEventListener( "load", function(){
+        if (xhr.readyState === 4) {
+
+          if (xhr.status === 200) {
+            requestOptions.success(xhr.data, xhr.statusText, xhr);
+          } else {
+            requestOptions.error(xhr, xhr.status, xhr.statusText);
+          }
+
+          requestOptions.complete(xhr, xhr.statusText);
+
+        }
     });
   }
 };
 
-$.ajax({
+// $.ajax({
 
+// });
+
+a = $.ajax({
+  complete: function() { console.log("complete" ); },
+  success: function(data, status) { console.log(status); },
+  error: function() { console.log("error" ); },
+  url: "http://reqres.in/api/users"
 });
 
 // complete (function) What arguments should this take?
