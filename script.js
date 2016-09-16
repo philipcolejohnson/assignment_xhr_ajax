15d73e37dc4b112907908964258f5152f20dad00
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-09-16 09:26:11 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// var xhr = new XMLHttpRequest();
+// xhr.addEventListener( "load", function(){
+//     console.log( this.responseText );
+// });
+// xhr.open("GET", "http://reqres.in/api/users", true);
+// xhr.send();
+
+
+// // Create a post
+// var xhr = new XMLHttpRequest();
+// xhr.addEventListener( "load", function(){
+//     console.log( this.responseText );
+// });
+// xhr.open("POST", "http://reqres.in/api/posts", true);
+// xhr.send("title=Foo&body=Bar&userId=1");
+
+var $ = {
+  ajax: function(options) {
+
+  }
+};
